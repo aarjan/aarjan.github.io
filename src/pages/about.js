e35b6a687319f6557541b893f7ae71a4cc931038
aarjan <baskotaaarjan@gmail.com>
2018-11-26 20:53:56 +0545

    jpt

---
+++ src/pages/about.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default () => (
+    <div style={{color:'teal'}}>
+        <h1>About site</h1>
+        <p>by:<h2>Aarjan</h2></p>
+    </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,28 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react"
 
-import Layout from '../components/layout'
-import Image from '../components/image'
-
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
+export default () => (
+  <div style={{margin: `3rem auto`, maxWidth:600}}>
+    <h1>Richard Hamming on Luck</h1>
+    <div>
+      <p>
+        From Richard Hamming’s classic and must-read talk, “
+        <a href="http://www.cs.virginia.edu/~robins/YouAndYourResearch.html">
+          You and Your Research
+        </a>
+        ”.
+      </p>
+      <blockquote>
+        <p>
+          There is indeed an element of luck, and no, there isn’t. The prepared
+          mind sooner or later finds something important and does it. So yes, it
+          is luck.{" "}
+          <em>
+            The particular thing you do is luck, but that you do something is
+            not.
+          </em>
+        </p>
+      </blockquote>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+    <p>Posted April 09, 2011</p>
+  </div>
 )
-
-export default IndexPage
